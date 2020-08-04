e9e1c537df16fbd71fbeef6056358686302283e3
DongWanLu <1848803291@qq.com>
2020-08-04 23:03:05 +0800

    day-04

--- jiyun-service-provider/src/main/java/cn/jiyun/service/userservice/JiyunServiceProviderApplication.java
+++ jiyun-service-provider/src/main/java/cn/jiyun/service/userservice/JiyunServiceProviderApplication.java
@@ -4,9 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-
 @SpringBootApplication
-@EnableEurekaClient
+@EnableEurekaClient //开启Eureka客户端
 public class JiyunServiceProviderApplication {
 
     public static void main(String[] args) {
--- jiyun-service-provider/src/main/java/cn/jiyun/service/userservice/controller/UserController.java
+++ jiyun-service-provider/src/main/java/cn/jiyun/service/userservice/controller/UserController.java
@@ -3,6 +3,7 @@ package cn.jiyun.service.userservice.controller;
 import cn.jiyun.service.userservice.pojo.User;
 import cn.jiyun.service.userservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +16,11 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Value("${server.port}")
+    private String port;
     @GetMapping("{id}")
     public User queryById(@PathVariable("id")Long id){
-
+        System.out.println("port:"+port);
         return this.userService.queryById(id);
     }
 }
